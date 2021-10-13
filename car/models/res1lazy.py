998636bd56bb0ea89a0434178d206a678eb253ca
Joshua Morris <joshuajohnmorris@gmail.com>
2021-10-13 18:59:04 +1000

    remove scenario class

--- car/models/res1lazy.py
+++ car/models/res1lazy.py
@@ -3,109 +3,115 @@ from car.utl.constants import PLLP_R1_GRID_WITH_BORDER
 import grblogtools as glt
 import matplotlib.pyplot as plt
 
-from .scenario import Scenario
+m = Model()
+m.setParam("LazyConstraints", 1)
+m.setParam("LogFile", "log.txt")
 
 
-class ResOneLazy(Scenario):
-    def __init__(self, name: str, **kwargs):
-        super().__init__(name, **kwargs)
-        self.e = 7
-        self.grid[0][self.e] = 0
+def in_bounds(position):
+    i, j = position
+    return 0 <= i < M and 0 <= j < N
 
-    def set_params(self):
-        super().set_params()
-        self.m.setParam("LazyConstraints", 1)
 
-    def set_variables(self):
-        self.X = {(i, j): self.m.addVar(vtype=GRB.BINARY) for i in range(self.M) for j in range(self.N)}
-        self.Y = {(i, j): self.m.addVar(vtype=GRB.BINARY) for i in range(self.M) for j in range(self.N)}
+def neighbors(i, j):
+    return list(filter(in_bounds, ((i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1))))
 
-    def set_objective(self):
-        self.m.setObjective(2 * quicksum(self.X[i, j] for i in range(self.M) for j in range(self.N)), GRB.MAXIMIZE)
 
-    def set_constraints(self):
-        # 3) street squares for entrance and exit
-        self.m.addConstr(self.Y[0, self.e] + self.Y[1, self.e] == 2)
+M = len(PLLP_R1_GRID_WITH_BORDER)
+N = len(PLLP_R1_GRID_WITH_BORDER[0])
 
-        # 5) connection of parking fields with driving lane
-        connectParkingFields = {
-            (i, j): self.m.addConstr(self.X[i, j] <= quicksum(self.Y[ii, jj] for ii, jj in self._neighbors(i, j)))
-            for i in range(1, self.M - 1)
-            for j in range(1, self.N - 1)
-        }
+# col of entrance square
+e = 7
+# entrance square is a street field
+PLLP_R1_GRID_WITH_BORDER[0][e] = 0
 
-        # 6) one type of field per square
-        oneFieldPerSquare = {
-            (i, j): self.m.addConstr(self.X[i, j] + self.Y[i, j] + PLLP_R1_GRID_WITH_BORDER[i][j] <= 1)
-            for i in range(self.M)
-            for j in range(self.N)
-        }
+X = {(i, j): m.addVar(vtype=GRB.BINARY) for i in range(M) for j in range(N)}
+Y = {(i, j): m.addVar(vtype=GRB.BINARY) for i in range(M) for j in range(N)}
 
-        # Each street field must be neighbors with one other street field
-        # (might be redundant after lazy constraints)
-        streetConnected = {
-            (i, j): self.m.addConstr(self.Y[i, j] <= quicksum(self.Y[ii, jj] for ii, jj in self._neighbors(i, j)))
-            for i in range(1, self.M - 1)
-            for j in range(1, self.N - 1)
-        }
+m.setObjective(2 * quicksum(X[i, j] for i in range(M) for j in range(N)), GRB.MAXIMIZE)
 
-    def optimize(self):
-        def callback(model, where):
-            if where != GRB.Callback.MIPSOL:
-                return
+# 3) street squares for entrance and exit
+m.addConstr(Y[0, e] + Y[1, e] == 2)
 
-            YV = model.cbGetSolution(self.Y)
-            # 1 for street, -1 for visited, 0 otherwise
-            grid = [[int(YV[i, j] >= 0.9) for j in range(self.N)] for i in range(self.M)]
+# 5) connection of parking fields with driving lane
+connectParkingFields = {
+    (i, j): m.addConstr(X[i, j] <= quicksum(Y[ii, jj] for ii, jj in neighbors(i, j)))
+    for i in range(1, M - 1)
+    for j in range(1, N - 1)
+}
 
-            regions = []
-            for i in range(self.M):
-                for j in range(self.N):
-                    if grid[i][j] == 1:
-                        regions.append(self._find_contiguous(i, j, grid))
+# 6) one type of field per square
+oneFieldPerSquare = {
+    (i, j): m.addConstr(X[i, j] + Y[i, j] + PLLP_R1_GRID_WITH_BORDER[i][j] <= 1)
+    for i in range(M)
+    for j in range(N)
+}
 
-            if len(regions) == 1:
-                # no disconnected regions
-                return
+# Each street field must be neighbors with one other street field
+# (might be redundant after lazy constraints)
+streetConnected = {
+    (i, j): m.addConstr(Y[i, j] <= quicksum(Y[ii, jj] for ii, jj in neighbors(i, j)))
+    for i in range(1, M - 1)
+    for j in range(1, N - 1)
+}
 
-            for region in regions:
-                region_neighbors = set()
-                for i, j in region:
-                    region_neighbors |= set(self._neighbors(i, j))
 
-                region_neighbors -= region
+def find_contiguous(i, j, grid):
+    tset = {(i, j)}
+    grid[i][j] = -1
+    for (ii, jj) in neighbors(i, j):
+        if grid[ii][jj] == 1:
+            tset |= find_contiguous(ii, jj, grid)
 
-                for i, j in region:
-                    model.cbLazy(self.Y[i, j] <= quicksum(self.Y[ii, jj] for ii, jj in region_neighbors))
+    return tset
 
-        self.m.optimize(callback)
 
-    def print_result(self):
-        for i in range(self.M):
-            for j in range(self.N):
-                square = "."
-                if self.X[i, j].x > 0.9:
-                    # parking
-                    square = "P"
-                elif self.Y[i, j].x > 0.9:
-                    # street
-                    square = "D"
+def callback(model, where):
+    if where != GRB.Callback.MIPSOL:
+        return
 
-                print(square, end="")
-            print()
+    YV = model.cbGetSolution(Y)
+    # 1 for street, -1 for visited, 0 otherwise
+    grid = [[int(YV[i, j] >= 0.9) for j in range(N)] for i in range(M)]
 
-    def _in_bounds(self, position):
-        i, j = position
-        return 0 <= i < self.M and 0 <= j < self.N
+    regions = []
+    for i in range(M):
+        for j in range(N):
+            if grid[i][j] == 1:
+                regions.append(find_contiguous(i, j, grid))
 
-    def _neighbors(self, i, j):
-        return list(filter(self._in_bounds, ((i + 1, j), (i - 1, j), (i, j + 1), (i, j - 1))))
+    if len(regions) == 1:
+        # no disconnected regions
+        return
 
-    def _find_contiguous(self, i, j, grid):
-        tset = {(i, j)}
-        grid[i][j] = -1
-        for (ii, jj) in self._neighbors(i, j):
-            if grid[ii][jj] == 1:
-                tset |= self._find_contiguous(ii, jj, grid)
+    for region in regions:
+        region_neighbors = set()
+        for i, j in region:
+            region_neighbors |= set(neighbors(i, j))
 
-        return tset
+        region_neighbors -= region
+
+        for i, j in region:
+            model.cbLazy(Y[i, j] <= quicksum(Y[ii, jj] for ii, jj in region_neighbors))
+
+
+m.optimize(callback)
+
+for i in range(M):
+    for j in range(N):
+        square = "."
+        if X[i, j].x > 0.9:
+            # parking
+            square = "P"
+        elif Y[i, j].x > 0.9:
+            # street
+            square = "D"
+
+        print(square, end="")
+    print()
+
+summary, timelines, rootlp = glt.get_dataframe(["log.txt"], timelines=True)
+timelines = timelines.fillna(0)
+
+fig, ax = plt.subplots()
+ax.plot(timelines.index, timelines.Gap)
