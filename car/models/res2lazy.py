9087b135e307d0264e112e81d86df700aa86fd45
Joshua Morris <joshuajohnmorris@gmail.com>
2021-10-20 10:25:23 +1000

    .

--- car/models/res2lazy.py
+++ car/models/res2lazy.py
@@ -1,195 +1,174 @@
 from gurobipy import *
 import copy
 
+from car.utl.constants import PLLP_R2_GRID
+
 E = 12
 
-grid = [
-    [1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-    [1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-    [1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-    [1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0],
-    [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-]
+M = len(PLLP_R2_GRID)
+N = len(PLLP_R2_GRID[0])
 
+def in_bounds(position):
+    i, j = position
+    return 0 <= i < M and 0 <= j < N
 
-def ResTwoLazy(grid, E, log_file=None):
-    grid = copy.deepcopy(grid)
+def dneighborsd(din):
+    res = set()
+    (a1, a2), (b1, b2), (c1, c2), (d1, d2) = din
 
-    M = len(grid)
-    N = len(grid[0])
+    res.add(((a1, a2 + 1), (b1, b2 + 1), (c1, c2 + 1), (d1, d2 + 1)))
+    res.add(((a1, a2 - 1), (b1, b2 - 1), (c1, c2 - 1), (d1, d2 - 1)))
+    res.add(((a1 + 1, a2), (b1 + 1, b2), (c1 + 1, c2), (d1 + 1, d2)))
+    res.add(((a1 - 1, a2), (b1 - 1, b2), (c1 - 1, c2), (d1 - 1, d2)))
 
-    def in_bounds(position):
-        i, j = position
-        return 0 <= i < M and 0 <= j < N
+    res2 = res.copy()
+    for d in res:
+        for i, j in d:
+            if not in_bounds((i, j)) and d in res2:
+                res2.remove(d)
 
-    def dneighborsd(din):
-        res = set()
-        (a1, a2), (b1, b2), (c1, c2), (d1, d2) = din
+    return list(res2)
 
-        res.add(((a1, a2 + 1), (b1, b2 + 1), (c1, c2 + 1), (d1, d2 + 1)))
-        res.add(((a1, a2 - 1), (b1, b2 - 1), (c1, c2 - 1), (d1, d2 - 1)))
-        res.add(((a1 + 1, a2), (b1 + 1, b2), (c1 + 1, c2), (d1 + 1, d2)))
-        res.add(((a1 - 1, a2), (b1 - 1, b2), (c1 - 1, c2), (d1 - 1, d2)))
+def dneighborsp(pfield):
+    res = set()
+    for dcur in D:
+        vertical = pfield[0][0] != pfield[1][0]
+        if (vertical):
+            neighbors = ((pfield[0][0] - 1, pfield[0][1]),
+                         (pfield[1][0] + 1, pfield[1][1]))
+        else:
+            neighbors = ((pfield[0][0], pfield[0][1] - 1),
+                         (pfield[1][0], pfield[1][1] + 1))
 
-        res2 = res.copy()
-        for d in res:
-            for i, j in d:
-                if not in_bounds((i, j)) and d in res2:
-                    res2.remove(d)
+        for n in list(filter(in_bounds, neighbors)):
+            if n in dcur:
+                res.add(dcur)
+    return list(res)
 
-        return list(res2)
+# COLUMN GENERATION
+def GenerateParkingFields():
+    P = []
+    for i in range(M - 1):
+        for j in range(N - 1):
+            P.append(((i, j), (i + 1, j)))
+            P.append(((i, j), (i, j + 1)))
+    return P
 
-    def dneighborsp(pfield):
-        res = set()
-        for dcur in D:
-            vertical = pfield[0][0] != pfield[1][0]
-            if (vertical):
-                neighbors = ((pfield[0][0] - 1, pfield[0][1]),
-                             (pfield[1][0] + 1, pfield[1][1]))
-            else:
-                neighbors = ((pfield[0][0], pfield[0][1] - 1),
-                             (pfield[1][0], pfield[1][1] + 1))
+def GenerateDrivingFields():
+    D = []
+    for i in range(M - 1):
+        for j in range(N - 1):
+            current = (i, j), (i, j + 1), (i + 1, j), (i + 1, j + 1)
+            D.append(current)
 
-            for n in list(filter(in_bounds, neighbors)):
-                if n in dcur:
-                    res.add(dcur)
-        return list(res)
+            if j == E and i == 0:
+                e = current
+    return D, e
 
-    # COLUMN GENERATION
-    def GenerateParkingFields():
-        P = []
-        for i in range(M - 1):
-            for j in range(N - 1):
-                P.append(((i, j), (i + 1, j)))
-                P.append(((i, j), (i, j + 1)))
-        return P
+P = GenerateParkingFields()
+D, e = GenerateDrivingFields()
 
-    def GenerateDrivingFields():
-        D = []
-        for i in range(M - 1):
-            for j in range(N - 1):
-                current = (i, j), (i, j + 1), (i + 1, j), (i + 1, j + 1)
-                D.append(current)
+_p = {(i, j, p):
+      1 if (i, j) in p else 0
+      for i in range(M) for j in range(N) for p in P}
 
-                if j == E and i == 0:
-                    e = current
-        return D, e
+_d = {(i, j, d):
+      1 if (i, j) in d else 0
+      for i in range(M) for j in range(N) for d in D}
 
-    P = GenerateParkingFields()
-    D, e = GenerateDrivingFields()
+# END COLUMN GENERATION
 
-    _p = {(i, j, p):
-          1 if (i, j) in p else 0
-          for i in range(M) for j in range(N) for p in P}
+m = Model()
+m.setParam("LazyConstraints", 1)
+m.setParam("LogFile", "res2lazy.txt")
 
-    _d = {(i, j, d):
-          1 if (i, j) in d else 0
-          for i in range(M) for j in range(N) for d in D}
+X = {p:
+     m.addVar(vtype=GRB.BINARY)
+     for p in P}
 
-    # END COLUMN GENERATION
+Y = {d:
+     m.addVar(vtype=GRB.BINARY)
+     for d in D}
 
-    m = Model()
-    m.setParam("LazyConstraints", 1)
-    m.setParam("LogFile", "res2lazy.txt")
+m.addConstr(Y[e] == 1)
 
-    X = {p:
-         m.addVar(vtype=GRB.BINARY)
-         for p in P}
+for i in range(M):
+    for j in range(N):
+        m.addConstr(quicksum(_p[i, j, p] * X[p] for p in P) <= 1)
 
-    Y = {d:
-         m.addVar(vtype=GRB.BINARY)
-         for d in D}
+for p in P:
+    m.addConstr(X[p]
+                <= quicksum(Y[d] for d in dneighborsp(p)))
 
-    m.addConstr(Y[e] == 1)
+for i in range(M):
+    for j in range(N):
+        m.addConstr(
+            quicksum(_p[i, j, p] * X[p] for p in P) +
+            quicksum(0.25 * _d[i, j, d] * Y[d] for d in D) +
+            PLLP_R2_GRID[i][j] <= 1
+        )
 
-    for i in range(M):
-        for j in range(N):
-            m.addConstr(quicksum(_p[i, j, p] * X[p] for p in P) <= 1)
+for d in D:
+    m.addConstr(Y[d]
+                <= quicksum(Y[dd] for dd in dneighborsd(d)))
 
-    for p in P:
-        m.addConstr(X[p]
-                    <= quicksum(Y[d] for d in dneighborsp(p)))
+m.setObjective(quicksum(X[p] for p in P), GRB.MAXIMIZE)
 
-    for i in range(M):
-        for j in range(N):
-            m.addConstr(
-                quicksum(_p[i, j, p] * X[p] for p in P) +
-                quicksum(0.25 * _d[i, j, d] * Y[d] for d in D) +
-                grid[i][j] <= 1
-            )
+def find_contiguous(d, driving_fields):
+    tset = {d}
+    driving_fields.remove(d)
+    for dd in dneighborsd(d):
+        if dd in driving_fields:
+            tset |= find_contiguous(dd, driving_fields)
 
-    for d in D:
-        m.addConstr(Y[d]
-                    <= quicksum(Y[dd] for dd in dneighborsd(d)))
+    return tset
 
-    m.setObjective(quicksum(X[p] for p in P), GRB.MAXIMIZE)
+def callback(model, where):
+    if where != GRB.Callback.MIPSOL:
+        return
 
-    def find_contiguous(d, driving_fields):
-        tset = {d}
-        driving_fields.remove(d)
-        for dd in dneighborsd(d):
-            if dd in driving_fields:
-                tset |= find_contiguous(dd, driving_fields)
+    YV = model.cbGetSolution(Y)
 
-        return tset
+    driving_fields = set()
+    for d in YV:
+        if YV[d] >= 0.9:
+            driving_fields.add(d)
 
-    def callback(model, where):
-        if where != GRB.Callback.MIPSOL:
-            return
+    regions = []
+    for d in driving_fields.copy():
+        if d in driving_fields:
+            regions.append(find_contiguous(d, driving_fields))
 
-        YV = model.cbGetSolution(Y)
+    if len(regions) == 1:
+        return
 
-        driving_fields = set()
-        for d in YV:
-            if YV[d] >= 0.9:
-                driving_fields.add(d)
+    for region in regions:
+        region_neighbors = set()
+        for d in region:
+            region_neighbors |= set(dneighborsd(d))
 
-        regions = []
-        for d in driving_fields.copy():
-            if d in driving_fields:
-                regions.append(find_contiguous(d, driving_fields))
+        region_neighbors -= region
 
-        if len(regions) == 1:
-            return
+        for d in region:
+            model.cbLazy(Y[d] <= quicksum(Y[dd]
+                         for dd in region_neighbors))
 
-        for region in regions:
-            region_neighbors = set()
-            for d in region:
-                region_neighbors |= set(dneighborsd(d))
+m.optimize(callback)
 
-            region_neighbors -= region
+count = 1
+for p in P:
+    if X[p].x > 0.9:
+        for i, j in p:
+            PLLP_R2_GRID[i][j] = -count
+            count += 1
+for d in D:
+    if Y[d].x > 0.9:
+        for i, j in d:
+            PLLP_R2_GRID[i][j] = 2
 
-            for d in region:
-                model.cbLazy(Y[d] <= quicksum(Y[dd]
-                             for dd in region_neighbors))
-
-    m.optimize(callback)
-
-    count = 1
-    for p in P:
-        if X[p].x > 0.9:
-            for i, j in p:
-                grid[i][j] = -count
-                count += 1
-    for d in D:
-        if Y[d].x > 0.9:
-            for i, j in d:
-                grid[i][j] = 2
-
-    for row in grid:
-        for c in row:
-            if c < 0:
-                c = "#"
-            print(c, end="")
-        print("")
-
-    return grid, m.objVal, m.Runtime
+for row in PLLP_R2_GRID:
+    for c in row:
+        if c < 0:
+            c = "#"
+        print(c, end="")
+    print("")
